7828531cb5b19dd17954e166a1e60e907e5c7ddf
Nils Berg <berg.nils@gmail.com>
2014-04-27 02:32:41 +0200

    who needs WSYNC anyway?

--- fencerdude.asm
+++ fencerdude.asm
@@ -4,9 +4,9 @@
 	include vcs.h
 	org $F000
 
-LowSwordOffset = 6
-MidSwordOffset = 9
-HighSwordOffset = 13
+LowSwordOffset = 7
+MidSwordOffset = 3
+HighSwordOffset = -1
 	
 P0YPosFromBot = $80;
 P0LinesLeft = $81;
@@ -42,6 +42,13 @@ P0Status = $93 		; Left nibble (D4-D7) is P0, right (D0-D3) P1
 	;;  D6: SwordThrown (if 1, don't touch P0MissileLine or HMM0 for Joystick events, sword is taken care of by physics - haha, like we have physics)
 	;;  D7: ResetPlayer (if 1, reset Player to his edge of the screen)
 P1Status = $94
+
+CurrentLine = $95
+PF0Base = $96
+PF1Base = $98
+PF2Base = $9A
+CurrentScreen = $9C 		; Incremented when moving one screen to the right, decremented when moving to the left
+				; Levels are symmetrical, so if CurrentScreen is negative, use NOT(CurrentScreen)+1 as screen index
 	
 ;generic start up stuff...
 Start
@@ -56,10 +63,14 @@ ClearMem
 	BNE ClearMem	
 	LDA #$00		
 	STA COLUBK	;start with black background
+	LDA #$82
+	STA COLUPF
 	LDA #66
 	STA COLUP0
 	LDA #$1E
 	STA COLUP1
+	LDA #1
+	STA CTRLPF
 ;Setting some variables...
 	LDA #40
 	STA P0YPosFromBot	;Initial Y Position
@@ -224,7 +235,6 @@ IsMid				; From mid to high...
 	LDA P0Status
 	AND #%11001111
 	ADC #%00100000
-
 	JMP ChangedStance
 IsHigh				; And back to low.
 	LDA P0Status
@@ -256,26 +266,27 @@ P0LowPos
 	STX P0Sprite
 	LDX #>FencerLow
 	STX P0Sprite+1
-	LDX #LowSwordOffset
-	STX P0MissileLine
+	LDA P0YPosFromBot
+	SBC #LowSwordOffset
 	JMP P0PosDone
 P0MidPos
 	LDX #<FencerMid
 	STX P0Sprite
 	LDX #>FencerMid
 	STX P0Sprite+1
-	LDX #MidSwordOffset
-	STX P0MissileLine
+	LDA P0YPosFromBot
+	SBC #MidSwordOffset
 	JMP P0PosDone
 P0HighPos
 	LDX #<FencerHigh
 	STX P0Sprite
 	LDX #>FencerHigh
 	STX P0Sprite+1
-	LDX #HighSwordOffset
-	STX P0MissileLine
+	LDA P0YPosFromBot
+	SBC #HighSwordOffset
 P0PosDone
-	
+	STA P0MissileLine
+	CLC
 P1PosStart
 	LDA #%00010000
 	BIT P1Status
@@ -288,26 +299,27 @@ P1LowPos
 	STX P1Sprite
 	LDX #>FencerLow
 	STX P1Sprite+1
-	LDX #LowSwordOffset
-	STX P1MissileLine
+	LDA P1YPosFromBot
+	SBC #LowSwordOffset
 	JMP P1PosDone
 P1MidPos
 	LDX #<FencerMid
 	STX P1Sprite
 	LDX #>FencerMid
 	STX P1Sprite+1
-	LDX #MidSwordOffset
-	STX P1MissileLine
+	LDA P1YPosFromBot
+	SBC #MidSwordOffset
 	JMP P1PosDone
 P1HighPos
 	LDX #<FencerHigh
 	STX P1Sprite
 	LDX #>FencerHigh
 	STX P1Sprite+1
-	LDX #HighSwordOffset
-	STX P1MissileLine
+	LDA P1YPosFromBot
+	SBC #HighSwordOffset
 P1PosDone
-
+	STA P1MissileLine
+	CLC
 P0SwordThrown
 P1SwordThrown			;TODO!
 
@@ -403,126 +415,157 @@ P1SwordSkip
 
 	STA WSYNC
 	STA HMOVE
+
+	;; For Testing only!
+	LDA #<PF0Center
+	STA PF0Base
+	LDA #>PF0Center
+	STA PF0Base+1
+	
+	LDA #<PF1Center
+	STA PF1Base
+	LDA #>PF1Center
+	STA PF1Base+1
+	
+	LDA #<PF2Center
+	STA PF2Base
+	LDA #>PF2Center
+	STA PF2Base+1
 	
 WaitForVblankEnd
 	LDA INTIM	
 	BNE WaitForVblankEnd	
 	LDX #95 		; Halved because we're now using a two-line kernel
+	STX CurrentLine
 	STA WSYNC
 	STA VBLANK  	
 
 
 ;main scanline loop...
-;
-;(this probably ends the "new code" section of today's
-; lesson...)
-
 
 ScanLoop 
-	STA WSYNC 		; 3 
+	
+	;; Entry point is at 3 cycles, since the jump from the end of the loop
+	;; happens right at the end of the line
+	STA ENAM0		; [3] + 3
+	STY ENAM1		; [6] + 3
+	LDA GRP0Next		; [9] + 3
+	STA GRP0		; [12] + 3
+	
+	LDA GRP1Next		; [15] + 3
+	STA GRP1		; [18] + 3
 
-	STY ENAM0
-	STA ENAM1
-	LDA GRP0Next		; 3
-	STA GRP0		; 3
+	;; All player and missile registers set after 21 cycles
 	
-	LDA GRP1Next		; 3
-	STA GRP1		; 3
-	;; Total cycles: 27
-	
-	;; Set ScanLoop timer - we just need to make sure
-	;; more than one scanline has passed at the end of the
-	;; kernel, so we don't need more resolution than 64 cycles
-	LDA #2			; 2
-	STA TIM64T 		; 4
+	;; PF0 and PF2 are set at the end of the loop
+	LDA PF1Next		; [21] + 3
+	STA PF1			; [24] + 3
+
+	LDX CurrentLine		; [27] + 3
+
+	;; From here on out, there are 12 "ghost cycles" from the timer I removed
+	;; but I don't want to update all cycle counts right now
 	
 ; here the idea is that P0LinesLeft
 ; is zero if the line isn't being drawn now,
 ; otherwise it's however many lines we have to go
 
-	LDA #14			; Only need to do this once
-CheckActivateP0
-	CPX P0YPosFromBot	; 3
-	BNE SkipActivateP0	; 2 (3 if taken)
-	STA P0LinesLeft		; 3
-SkipActivateP0
-	;; Total Cycles: 10
-CheckActivateP1
-	CPX P1YPosFromBot	; 3
-	BNE SkipActivateP1	; 2 (3)
-	STA P1LinesLeft		; 3
+	LDY #14			; [38] + 2 Only need to do this once
+CheckActivateP1			; arrive at [51]
+	CPX P1YPosFromBot	; [51] + 3
+	BNE SkipActivateP1	; [54] + 2 (3)
+	STY P1LinesLeft		; [56] + 3
+	JMP CheckActivateP0	; [59] + 3
 SkipActivateP1
+	NOP			; [57] + 2
+	STA $2D			; [59] + 3
 
+CheckActivateP0
+	CPX P0YPosFromBot	; [40] + 3
+	BNE SkipActivateP0	; [43] + 2 (3 if taken)
+	STY P0LinesLeft		; [45] + 3
+	JMP StartSpriteDrawing	; [48] + 3
+SkipActivateP0
+	NOP 			; [46] + 2
+	LDY P0LinesLeft		; [48] + 3 - storing ACC to nowhere
+
+StartSpriteDrawing		; [62]
 ;turn player graphics off then see if there's a line of sprite to draw
-	LDA #0			; 2
-	STA GRP0Next		; 3
-	STA GRP1Next		; 3
 
 	;; if P0LinesLeft is non zero,
 	;; we're drawing it
 
-	LDY P0LinesLeft 	; 3
-	BEQ FinishP0		; 2 (3 of taken)
+	;; Y contains P0LinesLeft
+
+	BEQ FinishP0		; [73] + 2 (3 if taken)
 IsP0_On
-	DEY			; 2
-	LDA (P0Sprite),Y	; 5 (6 if across page)
-	STA GRP0Next		; 3
-	STY P0LinesLeft		; 3
-	CPY P0MissileLine 	; 3
-	BNE DeactivateSwordP0	; 2 (3 if taken) - Don't activate Missile Register if not equal
-ActivateSwordP0
-	LDA #2			; 2
-	STA ENAM0Next		; 3
-	JMP FinishP0		; 3
-DeactivateSwordP0
-	LDA #0			; 2
-	STA ENAM0Next		; 3
-FinishP0
-
-	;; Total Cycles: 34
-
-	;; if P1LinesLeft is non zero,
-	;; we're drawing it
-
-	LDY P1LinesLeft 
-	BEQ FinishP1
-IsP1_On
-	DEY
-	LDA (P1Sprite),Y		
-	STA GRP1Next
-	STY P1LinesLeft
-	CPY P1MissileLine 	
-	BNE DeactivateSwordP1	; Don't activate Missile Register if not equal
-ActivateSwordP1
-	LDA #2
-	STA ENAM1Next
-	JMP FinishP1
-DeactivateSwordP1
-	LDA #0
-	STA ENAM1Next
-FinishP1
-
+	DEY			; [75] + 2
+	LDA (P0Sprite),Y	; [77] + 5
+	STA GRP0Next		; [82] + 3
+	STY P0LinesLeft		; [85] + 3
+	JMP DrawP1Sprite	; [88] + 3
+FinishP0			; [76]
+	DEC $2D			; [76] + 5
+	NOP
+	STY P0LinesLeft		; [81] + 3
+	LDA #0			; [86] + 2
+	STA GRP0Next		; [88] + 3
 	
-WaitForSecondLine
-	LDA INTIM
-	BNE WaitForSecondLine
+DrawP1Sprite			; [91]
+	CPX P0MissileLine	; [91] + 3
+	PHP			; [94] + 3 -
+	;; The status word is actually a valid input
+	;; to ENAM0/1, so that's cool
 
-	LDA ENAM1Next
-	LDY ENAM0Next
+	LDY P1LinesLeft 	; [97] + 3
+	BEQ FinishP1		; [100] + 2 (3)
+IsP1_On
+	DEY			; [102] + 2
+	LDA (P1Sprite),Y	; [104] + 5
+	STA GRP1Next		; [109] + 3
+	STY P1LinesLeft		; [112] + 3
+	JMP PlayersDone		; [115] + 3
+FinishP1			; [103]
+	DEC $2D			; [103] + 5
+	DEC $2D			; [108] + 5
+	LDA #0			; [113] + 2
+	STA GRP1Next		; [115] + 3
 
-	DEX			; 2
-	BNE ScanLoop		; 2 (3)
+PlayersDone			; [118] - 6 Ghost cycles
+	CPX P1MissileLine	; [112] + 3
+	PHP			; [115] + 3
 
+	LDY.w CurrentLine		; [118] + 3
+	DEC CurrentLine		; [121] + 2
+	BEQ StartOverscan	; [123] + 2 (3)
+	LDA (PF0Base),Y		; [125] + 5
+	TAX
+	LDA (PF1Base),Y		; [133] + 5
+	STA PF1Next		; [138] + 3
+	LDA (PF2Base),Y		; [141] + 5
+
+	STA PF2			; [146] + 3
+		
+	PLA			; 4
+	TAY
+	PLA			; 4
+	STX PF0			; [160] + 3
+	JMP ScanLoop		; [156, apparently] + 3
+
+StartOverscan
 	LDA #2		
 	STA WSYNC  	
 	STA VBLANK 	
-	LDY #31		
+	LDY #31
+	PLA			; These are here to make sure the stack doesn't overflow
+	PLA
 OverScanWait
 	STA WSYNC
 	DEY
 	BNE OverScanWait
 	JMP  MainLoop      
 
+	org $FD00
 FencerLow ; 14 Lines - Upside-Down because it's easier to draw that way
 	.byte %00100100  ;  X  X  ;
 	.byte %00100110  ;  X  XX ;
@@ -570,6 +613,298 @@ FencerMid
 	.byte %10100000  ;X X     ;
 	.byte %10110000  ;X XX    ;
 	.byte %10110000  ;X XX    ;
+
+PF0Center
+	.byte %11110000
+	.byte %11100000
+	.byte %11110000
+	.byte %11100000
+	.byte %11110000
+	.byte %11100000
+	.byte %11110000
+	.byte %11100000
+	.byte %11110000
+	.byte %11100000
+	.byte %11110000
+	.byte %11100000
+	.byte %00000000
+	.byte %11100000
+	.byte %00000000
+	.byte %11100000
+	.byte %00000000
+	.byte %11100000
+	.byte %00000000
+	.byte %11100000
+	.byte %00000000
+	.byte %11100000
+	.byte %00000000
+	.byte %11100000
+	.byte %00000000
+	.byte %11100000
+	.byte %00000000
+	.byte %11100000
+	.byte %00000000
+	.byte %11100000
+	.byte %00000000
+	.byte %11100000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %00000000
+	.byte %11110000
+	.byte %11110000
+	.byte %11110000
+	.byte %11110000
+	.byte %11110000
+	.byte %11110000
+	.byte %11110000
+	.byte %11110000
+
+	org $FE00
+PF1Center
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+	.byte %00111100
+
+PF2Center
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
+	.byte %11000110
 	
 	org $FFFC
 	.word Start		; NMI
